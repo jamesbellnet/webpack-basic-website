a91ae61e940e1fd2fa698e98bc3fd7d82998e833
jamesbellnet <james@jamesbell.net>
2019-11-18 11:35:18 +0000

    Switch autoprefixer for postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {
         plugins: [
-            require('autoprefixer')
+        require('postcss-preset-env')
         ]
     }
 }
